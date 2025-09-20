e20e4b3a692676c67164c3a5e63f5a75840c8c0b
fzaura <m3alemm0@gmail.com>
2025-09-20 04:26:44 +0300

    feat(validator): add validator function for validating user credentials when updating profile.
    validator works depending on which fields are included by the user to be updated.

--- server/validators/auth.validator.js
+++ server/validators/auth.validator.js
@@ -50,4 +50,54 @@ const loginValidator = [
   body("password").trim().notEmpty().withMessage("Password is required."),
 ];
 
-module.exports = { registerValidator, loginValidator };
+const updateUserValidator = [
+  body("username")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Username is required.")
+    .isLength({ max: 12, min: 5 })
+    .withMessage("Username has to be between 5 and 12 characters long.")
+    .escape(),
+
+  body("email")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required.")
+    .isEmail()
+    .withMessage("Please enter a valid email address.")
+    .normalizeEmail()
+    .escape(),
+
+  body("password")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Password is required.")
+    .isLength({ min: 10 })
+    .withMessage("Password cannot be shorter than 10 characters.")
+    .matches(/\d/)
+    .withMessage("Password must contain at least one number.")
+    .matches(/[!@#$%^&*(),.?":{}|<>]/)
+    .withMessage(
+      "Password must contain at least at least one special character."
+    ),
+
+  body("confirmPassword")
+    .trim()
+    .custom((value, { req }) => {
+      if (req.body.password) {
+        if (!value) {
+          throw new Error("Please re-enter your password for confirmation");
+        }
+        if (value !== req.body.password) {
+          throw new Error("Passwords do not match.");
+        }
+      }
+
+      return true;
+    }),
+];
+
+module.exports = { registerValidator, loginValidator, updateUserValidator };
