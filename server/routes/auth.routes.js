0f555e5a2d1868130349a8c411940ec30bbbdd28
fzaura <m3alemm0@gmail.com>
2025-09-22 04:06:20 +0300

    feat(auth): add get new access token to auth.routes.js

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -6,12 +6,19 @@ const {
   registerValidator,
   loginValidator,
   updateUserValidator,
+  refreshTokenValidator,
 } = require("../validators/auth.validator");
 const { authenticateJWT } = require("../middleware/auth.middleware");
 
 router.post("/register", registerValidator, authController.registerUser);
 
 router.post("/login", loginValidator, authController.loginUser);
+
+router.post(
+  "/access-token",
+  refreshTokenValidator,
+  authController.getNewAccessToken
+);
 
 router.put(
   "/update-user",
