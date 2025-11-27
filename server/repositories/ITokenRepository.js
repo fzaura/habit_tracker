3ecba26c459d8d28b786eaf77d3983b7fcb35772
fzaura <m3alemm0@gmail.com>
2025-11-27 15:41:13 +0300

    feat: create user and token repositories in server/repositories

---
+++ server/repositories/ITokenRepository.js
@@ -0,0 +1,17 @@
+class ITokenRepository {
+  createToken(userId, tokenValue) {
+    throw new Error("Method not implemented.");
+  }
+
+  findTokenByValue(tokenValue) {
+    throw new Error("Method not implemented.");
+  }
+
+  deleteTokenById(TokenId) {
+    throw new Error("Method not implemented.");
+  }
+
+  deleteTokenByUserId(userId) {
+    throw new Error("Method not implemented.");
+  }
+}
