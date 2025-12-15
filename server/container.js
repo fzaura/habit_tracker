4a5ebd377bec66d90b6be5118caff333c39942c0
fzaura <m3alemm0@gmail.com>
2025-12-15 23:58:10 +0300

    refactor(container): inject prisma client via awilix container

--- server/container.js
+++ server/container.js
@@ -6,6 +6,9 @@ const {
   asFunction,
 } = require("awilix");
 
+const prisma = require("./config/prisma");
+const mongoose = require("mongoose");
+
 const MongooseTokenRepo = require("./repositories/MongooseTokenRepository");
 const MongooseUserRepo = require("./repositories/MongooseUserRepository");
 const MongooseHabitRepo = require("./repositories/MongooseHabitRepository");
@@ -24,6 +27,8 @@ const createUserController = require("./controllers/user.controller");
 
 const container = createContainer();
 container.register({
+  db: asValue(prisma),
+
   tokenRepo: asClass(PrismaTokenRepo).scoped(),
   userRepo: asClass(PrismaUserRepo).scoped(),
   habitRepo: asClass(PrismaHabitRepo).scoped(),
