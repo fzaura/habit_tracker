8b451b2457bf0f776ab142b882734580937e38d8
fzaura <m3alemm0@gmail.com>
2026-01-02 22:20:04 +0300

    feat(repo): define deleteTokenById method in interface

--- server/repositories/ITokenRepository.ts
+++ server/repositories/ITokenRepository.ts
@@ -7,6 +7,7 @@ export interface ITokenRepository {
     expiresAt: Date
   ): Promise<RefreshToken>;
   findTokenByValue(token: string): Promise<RefreshToken | null>;
+  deleteTokenById(tokenId: string): Promise<RefreshToken>;
   deleteTokenByValue(token: string): Promise<RefreshToken>;
   deleteTokensByUserId(userId: string): Promise<number>;
 }
