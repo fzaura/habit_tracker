88c037340467a1ca00eb943495f6cc63cd8e7c55
Aser Hariri <AserAws2005@gmail.com>
2026-01-29 15:41:35 +0300

    feat(Added Both the new habits events and teh new habit States for BLoC why ? Because BLoC gets events and gives u state management changes)

--- mobile_frontend/habit_tracker_frontend/lib/presentation/Habits/BLoC/habits_event.dart
+++ mobile_frontend/habit_tracker_frontend/lib/presentation/Habits/BLoC/habits_event.dart
@@ -1,8 +1,48 @@
 import 'package:equatable/equatable.dart';
+import 'package:habit_tracker/domain/Habits/Entities/habitUI.dart';
 
 sealed class HabitsEvent extends Equatable {
   const HabitsEvent();
 
   @override
   List<Object?> get props => [];
+}
+
+sealed class HabitsLoadStarted extends HabitsEvent
+{
+
+}
+
+sealed class HabitsAddRequested extends HabitsEvent
+{
+  final Habit habit;
+  const HabitsAddRequested({required this.habit});
+  @override
+  List<Object?> get props => [habit];
+}
+
+
+sealed class HabitsDeleteRequested extends HabitsEvent
+{
+  final String habitId;
+  const HabitsDeleteRequested({required this.habitId});
+  @override
+  List<Object?> get props => [habitId];
+}
+
+sealed class HabitsUpdateRequested extends HabitsEvent
+{
+   final Habit habit;
+  const HabitsUpdateRequested({required this.habit});
+  @override
+  List<Object?> get props => [habit];
+}
+
+// 4. Triggered when toggling completion
+final class HabitsToggleRequested extends HabitsEvent {
+  final String id;
+  const HabitsToggleRequested(this.id);
+
+  @override
+  List<Object?> get props => [id];
 }
