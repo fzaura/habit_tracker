b86737c6d9eb119d11a11420da7bac1f775b360b
fzaura <m3alemm0@gmail.com>
2026-02-07 21:00:41 +0300

    refactor: replace zod schemas with class validator dtos and decorators

--- server/dtos/auth.dto.ts
+++ server/dtos/auth.dto.ts
@@ -1,19 +1,54 @@
 import { UserResponse } from "./user.dto";
+import {
+  IsEmail,
+  IsString,
+  Length,
+  IsNotEmpty,
+  MinLength,
+  Matches,
+} from "class-validator";
+import { Match } from "../match.decorator";
+import { Transform } from "class-transformer";
 
-export interface RegisterRequest {
-  username: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
+export class RegisterUserRequest {
+  @IsNotEmpty()
+  @IsString({ message: "Username must be a string." })
+  @Transform(({ value }) => value?.trim())
+  @Length(3, 10, { message: "Username must be between 3 and 10 characters." })
+  username!: string;
+
+  @IsEmail({}, { message: "Invalid email address format." })
+  @Transform(({ value }) => value?.trim())
+  email!: string;
+
+  @IsString()
+  @MinLength(8, { message: "Password must be at least 8 characters long." })
+  @Matches(/\d/, { message: "Password must contain at least one number." })
+  @Matches(/[!@#$%^&*(),.?":{}|<>]/, {
+    message: "Password must contain at least one special character.",
+  })
+  password!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: "Confirm password is required." })
+  @Match("password", { message: "Passwords do not match" })
+  confirmPassword!: string;
 }
 
-export interface LoginRequest {
-  email: string;
-  password: string;
+export class LoginRequest {
+  @IsEmail({}, { message: "Please provide a valid email." })
+  @Transform(({ value }) => value?.trim())
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: "Password is required." })
+  password!: string;
 }
 
-export interface RefreshUserSessionRequest {
-  refreshToken: string;
+export class RefreshUserSessionRequest {
+  @IsString()
+  @IsNotEmpty({ message: "Refresh token is required." })
+  refreshToken!: string;
 }
 
 export interface AuthResponse {
--- server/dtos/user.dto.ts
+++ server/dtos/user.dto.ts
@@ -1,11 +1,42 @@
 import { User } from "@prisma/client";
-export interface CreateUserRequest {
-  username: string;
-  email: string;
-  password: string;
-}
+import {
+  IsEmail,
+  IsString,
+  Length,
+  IsNotEmpty,
+  IsOptional,
+  MinLength,
+  Matches,
+  ValidateIf,
+} from "class-validator";
+import { Match } from "../match.decorator";
+import { Transform } from "class-transformer";
 
-export type UpdateUserRequest = Partial<CreateUserRequest>;
+export class UpdateUserRequest {
+  @IsOptional()
+  @IsString({ message: "Username must be a string." })
+  @Transform(({ value }) => value?.trim())
+  @Length(3, 10, { message: "Username must be between 3 and 10 characters." })
+  username?: string;
+
+  @IsOptional()
+  @IsEmail({}, { message: "Invalid email address format." })
+  @Transform(({ value }) => value?.trim())
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(8, { message: "Password must be at least 8 characters long." })
+  @Matches(/\d/)
+  @Matches(/[!@#$%^&*(),.?":{}|<>]/)
+  password?: string;
+
+  @ValidateIf((o) => o.password)
+  @IsString()
+  @IsNotEmpty({ message: "Confirm password is required." })
+  @Match("password", { message: "Passwords do not match" })
+  confirmPassword!: string;
+}
 
 export interface UserResponse {
   id: string;
